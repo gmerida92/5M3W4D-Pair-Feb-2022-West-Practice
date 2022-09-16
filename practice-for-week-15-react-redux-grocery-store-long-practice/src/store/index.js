5f1300076ec8fbea40ea6cc274333b4149742e0e
George Merida <gmerida92@gmail.com>
2022-09-15 22:03:54 -0700

    Phase 1 complete

---
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/index.js
@@ -0,0 +1,22 @@
+// ./src/store/index.js
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import produceReducer from './produce';
+
+const rootReducer = combineReducers({
+    producer: produceReducer
+});
+
+let enhancer;
+
+if(process.env.NODE_ENV !== "production") {
+    const logger = require("redux-logger").default;
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+}
+
+
+export default configureStore;
---
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
@@ -0,0 +1,7 @@
+// ./src/store/produce.js
+export default function produceReducer(state = {}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
